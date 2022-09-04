5ada48d72ae32a089c962c300adaa367466ba801
theswayz33xpress <112774351+theswayz33xpress@users.noreply.github.com>
2022-09-04 16:31:31 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let totalEl = document.getElementById("total-el")
+let count = 0
+let total = 0
+
+function increment() {
+    count += 1
+    total += 1
+    countEl.textContent = count
+    totalEl.textContent = "Total: " + total
+}
+
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
+}
+
+
